ee3cd09ced910a9d1c99a40986369f2d5b0c4c34
b4ry <barry90@interia.pl>
2024-06-27 14:39:24 +0200

    - add an option to initialize map size

--- map.rb
+++ map.rb
@@ -1,77 +1,87 @@
 class Map
-    def initialize(rows = 10, columns = 10)
-        @rows = rows
-        @columns = columns
+    def initialize()        
+        begin
+            puts("Provide map width (min 3): ")
+            @columns = gets.to_i
+        end while @columns < 3
+
+        begin
+            puts("Provide map height (min 3): ")
+            @rows = gets.to_i
+        end while @rows < 3
+
         @map = Array.new(@rows) { Array.new(@columns) }
-        @map_objects = []
+        @map_objects = {}
+    end
 
-        def draw_map(current_object)
-            clear_screen
+    def draw_map(current_object)
+        clear_screen
+        print "\n"
+
+        reset_map
+        place_objects
+        draw_object_vision_map(current_object)
+    end
+    
+    def draw_object_vision_map(current_object)
+        current_object_row = current_object.get_position.row
+        current_object_col = current_object.get_position.col
+        current_object_fov = current_object.get_fov
+
+        current_object_fov_row_min = current_object_row - current_object_fov;
+        current_object_fov_row_max = current_object_row + current_object_fov;
+        current_object_fov_col_min = current_object_col - current_object_fov;
+        current_object_fov_col_max = current_object_col + current_object_fov;
+
+        fov_row_min = current_object_fov_row_min < 0 ? 0 : current_object_fov_row_min;
+        fov_row_max = current_object_fov_row_max < @rows ? current_object_fov_row_max : @rows-1;
+        fov_col_min = current_object_fov_col_min < 0 ? 0 : current_object_fov_col_min;
+        fov_col_max = current_object_fov_col_max < @columns ? current_object_fov_col_max : @columns-1;
+
+        for row in fov_row_min..fov_row_max do
+            for col in fov_col_min..fov_col_max do
+                print @map[row][col]
+            end
             print "\n"
-
-            reset_map
-            place_objects
-            draw_object_vision_map(current_object)
-        end
-        
-        def draw_object_vision_map(current_object)
-            current_object_row = current_object.get_position.row
-            current_object_col = current_object.get_position.col
-            current_object_fov = current_object.get_fov
-
-            current_object_fov_row_min = current_object_row - current_object_fov;
-            current_object_fov_row_max = current_object_row + current_object_fov;
-            current_object_fov_col_min = current_object_col - current_object_fov;
-            current_object_fov_col_max = current_object_col + current_object_fov;
-
-            fov_row_min = current_object_fov_row_min < 0 ? 0 : current_object_fov_row_min;
-            fov_row_max = current_object_fov_row_max >= 9 ? 9 : current_object_fov_row_max;
-            fov_col_min = current_object_fov_col_min < 0 ? 0 : current_object_fov_col_min;
-            fov_col_max = current_object_fov_col_max >= 9 ? 9 : current_object_fov_col_max;
-
-            for row in fov_row_min..fov_row_max do
-                for col in fov_col_min..fov_col_max do
-                    print @map[row][col]
-                end
-                print "\n"
-            end
-
-            print "\n"
-            puts("Current coordinates: x - #{current_object_row}, y - #{current_object_col}, fov (field of vision) - #{current_object_fov}")
-        end
-        def reset_map
-            @map = Array.new(@rows) { Array.new(@columns, ".") }
-
-            for x in 0..9 do
-                @map[x][0] = "|"
-                @map[x][9] = "|"
-            end
-
-            for y in 1..8 do
-                @map[0][y] = "-"
-                @map[9][y] = "-"
-            end
         end
 
-        def place_objects
-            @map_objects.each do |obj|
-                position = obj.get_position
-                @map[position.row][position.col] = obj.to_s
-            end
+        print "\n"
+        puts("Current coordinates: x - #{current_object_row}, y - #{current_object_col}, fov (field of vision) - #{current_object_fov}")
+    end
+    
+    def reset_map
+        @map = Array.new(@rows) { Array.new(@columns, ".") }
+
+        last_row_index = @rows-1
+        last_col_index = @columns-1
+
+        for x in 0..last_row_index do
+            @map[x][0] = "|"
+            @map[x][last_col_index] = "|"
         end
 
-        def add_object(obj)
-            @map_objects[obj.get_id] = obj
-            position = obj.get_position
-            @map[position.row][position.col] = obj.to_s
+        for y in 1..last_col_index-1 do
+            @map[0][y] = "-"
+            @map[last_row_index][y] = "-"
         end
+    end
 
-        def clear_screen
-            if RUBY_PLATFORM =~ /win32|win64|mingw/
-              system("cls")  # Windows
-            else
-              print "\e[2J\e[f"  # Unix-like
-            end
+    def place_objects
+        @map_objects.each do |key, value|
+            position = value.get_position
+            @map[position.row][position.col] = value.to_s
+        end
+    end
+
+    def add_object(obj)
+        @map_objects[obj.get_id] = obj
+    end
+
+    def clear_screen
+        if RUBY_PLATFORM =~ /win32|win64|mingw/
+          system("cls")  # Windows
+        else
+          print "\e[2J\e[f"  # Unix-like
         end
     end
 end
