a27a3140735361cb14e3cb93aa3ff115fa835cbb
b4ry <barry90@interia.pl>
2024-07-05 17:32:59 +0200

    - fix indentations

--- map.rb
+++ map.rb
@@ -69,43 +69,43 @@ class Map
     def generate_perlin_map
         perlin_noise = PerlinNoise.new()
           
-          rand_amp = rand() * 0.15 + 0.95
-          rand_freq = rand() * 0.006 + 0.006
-          rand_seed = rand(-10000..1000)
+        rand_amp = rand() * 0.15 + 0.95
+        rand_freq = rand() * 0.006 + 0.006
+        rand_seed = rand(-10000..1000)
   
-          perlin_noise.generate_fbm_perlin_noise(@map, rand_amp, rand_freq, 8, rand_seed)
+        perlin_noise.generate_fbm_perlin_noise(@map, rand_amp, rand_freq, 8, rand_seed)
   
-          @map.each_with_index do |row, i|
-              row.each_with_index do |col, j|
-                  if(@map[i][j]) < -0.24
-                      @map[i][j] = "#{COLORS[DARK_BLUE]} .#{COLORS[RESET]}" # deep water
-                  elsif(@map[i][j]) < 0.07
-                      @map[i][j] = "#{COLORS[BLUE]} .#{COLORS[RESET]}" # water
-                  elsif(@map[i][j]) < 0.1
-                      @map[i][j] = "#{COLORS[YELLOW]} .#{COLORS[RESET]}" # shore
-                  elsif(@map[i][j]) < 0.15
-                      @map[i][j] = "#{COLORS[LIGHT_GREEN]} .#{COLORS[RESET]}" # marsh
-                  elsif(@map[i][j]) < 0.30
-                      @map[i][j] = "#{COLORS[GREEN]} .#{COLORS[RESET]}" # grass
-                  elsif(@map[i][j]) < 0.42
-                      @map[i][j] = "#{COLORS[BROWN]} .#{COLORS[RESET]}" # hills
-                  elsif(@map[i][j]) < 0.55
-                      @map[i][j] = "#{COLORS[WHITE]} .#{COLORS[RESET]}" # mountains
-                  else
-                      @map[i][j] = "#{COLORS[BLACK]} .#{COLORS[RESET]}" # summit
-                  end
+        @map.each_with_index do |row, i|
+            row.each_with_index do |col, j|
+                if(@map[i][j]) < -0.24
+                    @map[i][j] = "#{COLORS[DARK_BLUE]} .#{COLORS[RESET]}" # deep water
+                elsif(@map[i][j]) < 0.07
+                    @map[i][j] = "#{COLORS[BLUE]} .#{COLORS[RESET]}" # water
+                elsif(@map[i][j]) < 0.1
+                    @map[i][j] = "#{COLORS[YELLOW]} .#{COLORS[RESET]}" # shore
+                elsif(@map[i][j]) < 0.15
+                    @map[i][j] = "#{COLORS[LIGHT_GREEN]} .#{COLORS[RESET]}" # marsh
+                elsif(@map[i][j]) < 0.30
+                    @map[i][j] = "#{COLORS[GREEN]} .#{COLORS[RESET]}" # grass
+                elsif(@map[i][j]) < 0.42
+                    @map[i][j] = "#{COLORS[BROWN]} .#{COLORS[RESET]}" # hills
+                elsif(@map[i][j]) < 0.55
+                    @map[i][j] = "#{COLORS[WHITE]} .#{COLORS[RESET]}" # mountains
+                else
+                    @map[i][j] = "#{COLORS[BLACK]} .#{COLORS[RESET]}" # summit
+                end
   
-                  @unaltered_map[i][j] = @map[i][j]
+                @unaltered_map[i][j] = @map[i][j]
   
-                  without_space = @map[i][j].sub(" .", ".")
-                  print(without_space)
-              end
+                without_space = @map[i][j].sub(" .", ".")
+                print(without_space)
+            end
   
-              puts
-          end
+            puts
+        end
   
-          TerminalHelper.clear_screen()
-      end
+        TerminalHelper.clear_screen()
+    end
 
     def draw_object_vision_map(current_object)
         TerminalHelper.go_to(0, 0)
