ce950f343a86513a6aa6111e2f0fa048ca451c88
b4ry <barry90@interia.pl>
2024-07-05 17:31:45 +0200

    - refactor, - fix clearing the screen after map borders

--- map.rb
+++ map.rb
@@ -7,66 +7,16 @@ class Map
     attr_accessor :map_objects
            
     def initialize()
-        @columns = 225
-        @rows = 225
+        @columns = 10
+        @rows = 10
 
         @map = Array.new(@rows) { Array.new(@columns) }
         @unaltered_map = Array.new(@rows) { Array.new(@columns) }
 
-        perlin_noise = PerlinNoise.new()
-        rand_amp = rand() * 0.15 + 0.95
-        rand_freq = rand() * 0.006 + 0.006
-        rand_seed = rand(-10000..1000)
-
-        perlin_noise.generate_fbm_perlin_noise(@map, rand_amp, rand_freq, 8, rand_seed)
-
-        @map.each_with_index do |row, i|
-            row.each_with_index do |col, j|
-                if(@map[i][j]) < -0.24
-                    @map[i][j] = "#{COLORS[DARK_BLUE]} .#{COLORS[RESET]}" # deep water
-                elsif(@map[i][j]) < 0.07
-                    @map[i][j] = "#{COLORS[BLUE]} .#{COLORS[RESET]}" # water
-                elsif(@map[i][j]) < 0.1
-                    @map[i][j] = "#{COLORS[YELLOW]} .#{COLORS[RESET]}" # shore
-                elsif(@map[i][j]) < 0.15
-                    @map[i][j] = "#{COLORS[LIGHT_GREEN]} .#{COLORS[RESET]}" # marsh
-                elsif(@map[i][j]) < 0.30
-                    @map[i][j] = "#{COLORS[GREEN]} .#{COLORS[RESET]}" # grass
-                elsif(@map[i][j]) < 0.42
-                    @map[i][j] = "#{COLORS[BROWN]} .#{COLORS[RESET]}" # hills
-                elsif(@map[i][j]) < 0.55
-                    @map[i][j] = "#{COLORS[WHITE]} .#{COLORS[RESET]}" # mountains
-                else
-                    @map[i][j] = "#{COLORS[BLACK]} .#{COLORS[RESET]}" # summit
-                end
-
-                @unaltered_map[i][j] = @map[i][j]
-
-                without_space = @map[i][j].sub(" .", ".")
-                print(without_space)
-            end
-
-            puts
-        end
-
-        TerminalHelper.clear_screen()
+        generate_perlin_map()
         @map_objects = {}
 
-        for x in 0..@rows-1 do
-            verticalBorderLeft = Border.new(x, 0, true)
-            add_map_object(verticalBorderLeft)
-    
-            verticalBorderRight = Border.new(x, @columns-1, true)
-            add_map_object(verticalBorderRight)
-        end
-    
-        for y in 1..@columns-2 do
-            horizontalBorderUp = Border.new(0, y, false)
-            add_map_object(horizontalBorderUp)
-    
-            horizontalBorderDown = Border.new(@rows-1, y, false)
-            add_map_object(horizontalBorderDown)
-        end
+        generate_map_borders()
     end
 
     def draw_map(current_object)
@@ -82,7 +32,80 @@ class Map
         @map[row][col] = @unaltered_map[row][col]
     end
     
-    private 
+    private
+
+    def generate_map_borders
+        progress = 0.0
+
+        for x in 0..@rows-1 do
+            verticalBorderLeft = Border.new(x, 0, true)
+            add_map_object(verticalBorderLeft)
+      
+            verticalBorderRight = Border.new(x, @columns-1, true)
+            add_map_object(verticalBorderRight)
+
+            TerminalHelper.go_to(0, 0)
+            progress += 1
+            puts("Generating vertical map borders: #{((progress / @rows) * 100).to_i} %")
+        end
+      
+        progress = 0.0
+
+        for y in 1..@columns-2 do
+            horizontalBorderUp = Border.new(0, y, false)
+            add_map_object(horizontalBorderUp)
+      
+            horizontalBorderDown = Border.new(@rows-1, y, false)
+            add_map_object(horizontalBorderDown)
+
+            TerminalHelper.go_to(0, 0)
+            progress += 1
+            puts("Generating horizontal map borders: #{((progress / @columns) * 100).to_i} %")
+        end
+
+        TerminalHelper.clear_screen()
+    end
+
+    def generate_perlin_map
+        perlin_noise = PerlinNoise.new()
+          
+          rand_amp = rand() * 0.15 + 0.95
+          rand_freq = rand() * 0.006 + 0.006
+          rand_seed = rand(-10000..1000)
+  
+          perlin_noise.generate_fbm_perlin_noise(@map, rand_amp, rand_freq, 8, rand_seed)
+  
+          @map.each_with_index do |row, i|
+              row.each_with_index do |col, j|
+                  if(@map[i][j]) < -0.24
+                      @map[i][j] = "#{COLORS[DARK_BLUE]} .#{COLORS[RESET]}" # deep water
+                  elsif(@map[i][j]) < 0.07
+                      @map[i][j] = "#{COLORS[BLUE]} .#{COLORS[RESET]}" # water
+                  elsif(@map[i][j]) < 0.1
+                      @map[i][j] = "#{COLORS[YELLOW]} .#{COLORS[RESET]}" # shore
+                  elsif(@map[i][j]) < 0.15
+                      @map[i][j] = "#{COLORS[LIGHT_GREEN]} .#{COLORS[RESET]}" # marsh
+                  elsif(@map[i][j]) < 0.30
+                      @map[i][j] = "#{COLORS[GREEN]} .#{COLORS[RESET]}" # grass
+                  elsif(@map[i][j]) < 0.42
+                      @map[i][j] = "#{COLORS[BROWN]} .#{COLORS[RESET]}" # hills
+                  elsif(@map[i][j]) < 0.55
+                      @map[i][j] = "#{COLORS[WHITE]} .#{COLORS[RESET]}" # mountains
+                  else
+                      @map[i][j] = "#{COLORS[BLACK]} .#{COLORS[RESET]}" # summit
+                  end
+  
+                  @unaltered_map[i][j] = @map[i][j]
+  
+                  without_space = @map[i][j].sub(" .", ".")
+                  print(without_space)
+              end
+  
+              puts
+          end
+  
+          TerminalHelper.clear_screen()
+      end
 
     def draw_object_vision_map(current_object)
         TerminalHelper.go_to(0, 0)
@@ -141,11 +164,24 @@ class Map
 
         TerminalHelper.clear_line(fov_row_max + 2, 0)
 
-        cursor_col = current_object_col <= 4 ? (current_object_fov_col_max+2)*2 : 24
+        cursor_col = current_object_col <= 4 ? (current_object_fov_col_max + 2) * 2 : 24
+
+        if((@columns - 1) == fov_col_max)
+            cursor_col = current_object_fov * 2 + 4 + (@columns - 1 - current_object_col) * 2 # + 4 because player and a border. They are DOUBLED, because of " " in when rendering, eg " @"
+        end
 
         0.upto(fov_row_max) do |row|
             TerminalHelper.go_to(row + 1, cursor_col)
             print "  "
+        end
+
+        if((@rows - 1) == fov_row_max)
+            cursor_row = current_object_fov + 2 + (@rows - 1 - current_object_row) # rows are not DOUBLED so no need for * 2
+
+            0.upto(cursor_col) do |col|
+                TerminalHelper.go_to(cursor_row, col + 1)
+                print "  "
+            end
         end
 
         TerminalHelper.go_to(0, 30)
